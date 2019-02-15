7dd4e233e7e1943affaa09670e9d12c223a46ef5
Nahumkelly <46894120+Nahumkelly@users.noreply.github.com>
2019-02-15 15:12:11 -0500

    VTDI Gate Application

---
+++ VDTI_Application/LogIn.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace VDTI_Application
+{
+    partial class LogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lbusername = new System.Windows.Forms.Label();
+            this.lbpassword = new System.Windows.Forms.Label();
+            this.tbusername = new System.Windows.Forms.TextBox();
+            this.tbpassword = new System.Windows.Forms.TextBox();
+            this.lblogin = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btnreset = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.77811F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63.22189F));
+            this.tableLayoutPanel1.Controls.Add(this.tbpassword, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbusername, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lbpassword, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tbusername, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(104, 106);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(329, 100);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // lbusername
+            // 
+            this.lbusername.AutoSize = true;
+            this.lbusername.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbusername.ForeColor = System.Drawing.Color.White;
+            this.lbusername.Location = new System.Drawing.Point(3, 0);
+            this.lbusername.Name = "lbusername";
+            this.lbusername.Size = new System.Drawing.Size(111, 22);
+            this.lbusername.TabIndex = 0;
+            this.lbusername.Text = "User Name";
+            // 
+            // lbpassword
+            // 
+            this.lbpassword.AutoSize = true;
+            this.lbpassword.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbpassword.ForeColor = System.Drawing.Color.White;
+            this.lbpassword.Location = new System.Drawing.Point(3, 50);
+            this.lbpassword.Name = "lbpassword";
+            this.lbpassword.Size = new System.Drawing.Size(100, 22);
+            this.lbpassword.TabIndex = 1;
+            this.lbpassword.Text = "Password";
+            // 
+            // tbusername
+            // 
+            this.tbusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbusername.Location = new System.Drawing.Point(123, 3);
+            this.tbusername.Name = "tbusername";
+            this.tbusername.Size = new System.Drawing.Size(202, 26);
+            this.tbusername.TabIndex = 2;
+            // 
+            // tbpassword
+            // 
+            this.tbpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbpassword.Location = new System.Drawing.Point(123, 53);
+            this.tbpassword.Name = "tbpassword";
+            this.tbpassword.PasswordChar = '*';
+            this.tbpassword.Size = new System.Drawing.Size(199, 26);
+            this.tbpassword.TabIndex = 3;
+            // 
+            // lblogin
+            // 
+            this.lblogin.AutoSize = true;
+            this.lblogin.Font = new System.Drawing.Font("Blackoak Std", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblogin.Location = new System.Drawing.Point(115, 25);
+            this.lblogin.Name = "lblogin";
+            this.lblogin.Size = new System.Drawing.Size(175, 28);
+            this.lblogin.TabIndex = 1;
+            this.lblogin.Text = "Log In";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::VDTI_Application.Properties.Resources.key2;
+            this.pictureBox1.Location = new System.Drawing.Point(-13, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 132);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlogin.Location = new System.Drawing.Point(107, 221);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(87, 33);
+            this.btnlogin.TabIndex = 3;
+            this.btnlogin.Text = "Log In";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            // 
+            // btnreset
+            // 
+            this.btnreset.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnreset.Location = new System.Drawing.Point(292, 221);
+            this.btnreset.Name = "btnreset";
+            this.btnreset.Size = new System.Drawing.Size(87, 33);
+            this.btnreset.TabIndex = 4;
+            this.btnreset.Text = "Reset";
+            this.btnreset.UseVisualStyleBackColor = true;
+            // 
+            // LogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(443, 289);
+            this.Controls.Add(this.btnreset);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblogin);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "LogIn";
+            this.Text = "LogIn";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox tbpassword;
+        private System.Windows.Forms.Label lbusername;
+        private System.Windows.Forms.Label lbpassword;
+        private System.Windows.Forms.TextBox tbusername;
+        private System.Windows.Forms.Label lblogin;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btnreset;
+    }
+}
